523691f2039b37f7d3235d7e85f371c62ddf48d0
iqbal <iqbal.bd@asa-international.com>
2025-06-16 12:22:09 +0600

    hfytfyf

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InN0cmluZyIsIm5iZiI6MTc1MDA1MzEwNywiZXhwIjoxNzUwMDU2NzA3LCJpYXQiOjE3NTAwNTMxMDd9._4NLKZ81mgI_uBNowt3bu4kdceXMOqYz_rb5EdSc4d0');
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
